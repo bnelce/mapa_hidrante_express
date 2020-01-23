8469aa72c28f9d01af97f80fbff4b8e387c60519
Abner Augusto Lima de Oliveira <abner.oliveira.ce@gmail.com>
2020-01-23 11:53:20 -0300

    backup Barboza

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,25 @@
-module.exports = {
+//postgres
+/*module.exports = {
   dialect: 'postgres',
   host: 'localhost',
   username: 'postgres',
   password: 'docker',
   database: 'barber',
+  define: {
+    // garante que tem uma coluna createUp e updateAt dentro de cada tabela do banco de dados
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};*/
+
+//mysql
+module.exports = {
+  dialect: 'mysql',
+  host: 'localhost',
+  username: 'root',
+  password: '',
+  database: 'sabm',
   define: {
     // garante que tem uma coluna createUp e updateAt dentro de cada tabela do banco de dados
     timestamps: true,
