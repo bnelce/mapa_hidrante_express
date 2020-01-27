3641b1c496ec2d579364f76b446ce68b17f2f4a5
Abner Augusto Lima de Oliveira <abner.oliveira.ce@gmail.com>
2020-01-27 15:52:01 -0300

    umbler

--- src/config/database.js
+++ src/config/database.js
@@ -11,7 +11,7 @@
     underscored: true,
     underscoredAll: true,
   },
-};*/
+};
 
 //mysql
 module.exports = {
@@ -26,4 +26,20 @@ module.exports = {
     underscored: true,
     underscoredAll: true,
   },
+};*/
+
+//umbler
+module.exports = {
+  dialect: 'mysql',
+  host: 'localhost',
+  username: 'sabm',
+  password: 'sabm1234',
+  database: 'sabm',
+  define: {
+    // garante que tem uma coluna createUp e updateAt dentro de cada tabela do banco de dados
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
 };
+
