424087743599baea693520d15e788e9b55318f80
Abner Augusto Lima de Oliveira <abner.oliveira.ce@gmail.com>
2020-01-27 17:55:10 -0300

    require

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,5 @@
 //import Sequelize from 'sequelize';
-const Sequelize = requeire('sequelize');
+const Sequelize = require('sequelize');
 
 import SecUser from '../app/models/SecUser';
 import Files from '../app/models/Files';
