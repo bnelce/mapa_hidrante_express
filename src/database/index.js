d27be69a4470f0d467f4c540df6e7db1d9192a01
Abner Oliveira <abner.oliveira.ce@gmail.com>
2020-03-17 10:43:46 -0300

    commit pro 12

--- src/database/index.js
+++ src/database/index.js
@@ -5,10 +5,11 @@ import Hidrantes from '../app/models/Hidrantes';
 import SecUser from '../app/models/SecUser';
 import Vistoria from '../app/models/Vistoria';
 import Files from '../app/models/Files';
+import Boletim from '../app/models/sentinela/Boletim';
 
 import databaseConfig from '../config/database';
 
-const models = [SecUser, Hidrantes, Files, Vistoria];
+const models = [SecUser, Hidrantes, Files, Vistoria, Boletim];
 
 class Database {
   constructor() {
